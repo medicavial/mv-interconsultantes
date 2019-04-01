8fdcaa0eb0e33dc280bc58ef7a905998eed73b23
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-04-01 17:47:20 -0600

    CONEXION A BASE DE DIRECTORIOS

--- server/config/database.php
+++ server/config/database.php
@@ -100,6 +100,18 @@ return [
 			'strict'    => false,
 		],
 
+		'directorios' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE_DIRECTORIOS', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
